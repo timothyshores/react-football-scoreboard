aa2d1a26fcecc8daf0bf5908996628cc85047c6d
Timothy Shores <timothymshores@gmail.com>
2019-08-20 09:46:02 -0500

    Remove TODO comments

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
-	//TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 	const [homeScore, setHomeScore] = useState(0);
 	const [awayScore, setAwayScore] = useState(0);
 
@@ -14,9 +13,6 @@ function App() {
 				<div className="topRow">
 					<div className="home">
 						<h2 className="home__name">Lions</h2>
-
-						{/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
 						<div className="home__score">{homeScore}</div>
 					</div>
 					<div className="timer">00:03</div>
@@ -29,7 +25,6 @@ function App() {
 			</section>
 			<section className="buttons">
 				<div className="homeButtons">
-					{/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
 					<button
 						className="homeButtons__touchdown"
 						onClick={() => setHomeScore(homeScore + 7)}
